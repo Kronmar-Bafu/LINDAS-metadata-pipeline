8a19ca8fb7b274a7650074337175bf7b701b317c
{'changed': False, 'rc': 0, 'stderr': '', 'stdout': 'Paused for 0.19 minutes', 'start': '2024-06-17 12:06:43.482554', 'stop': '2024-06-17 12:06:54.851195', 'delta': 11, 'echo': True, 'user_input': 'Marco Kronenberg', 'failed': False} <{'changed': False, 'rc': 0, 'stderr': '', 'stdout': 'Paused for 0.03 minutes', 'start': '2024-06-17 12:06:54.886231', 'stop': '2024-06-17 12:06:56.618968', 'delta': 1, 'echo': True, 'user_input': '', 'failed': False}>
2024-06-25 14:51:50 +0200

    Small refactoring for namespaces.py

--- namespaces.py
+++ namespaces.py
@@ -1,6 +1,22 @@
-from rdflib import Namespace
+from rdflib import Graph, Namespace
 
 
+CUBE = Namespace("https://cube.link/")
 DCAT = Namespace("http://www.w3.org/ns/dcat#")
 DCT = Namespace("http://purl.org/dc/terms/")
 SCHEMA = Namespace("http://schema.org/")
+SH = Namespace("http://www.w3.org/ns/shacl#")
+
+
+Namespaces = {
+    "cube": CUBE,
+    "dcat": DCAT,
+    "dct": DCT,
+    "schema": SCHEMA
+}
+
+
+def bind_namespaces(graph: Graph) -> Graph:
+    for prefix, nmspc in Namespaces.items():
+        graph.bind(prefix=prefix, namespace=nmspc)
+    return graph
